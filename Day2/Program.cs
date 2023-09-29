3d0ba788021fd5256ff8083d8be16bc75e99c939
Gonule1 <Gnlelmali33@gmail.com>
2023-09-29 14:55:10 +0300

    Proje dosyası ekle.

---
+++ Day2/Program.cs
@@ -0,0 +1,84 @@
+﻿
+/*
+Console.WriteLine("lütfen yaşınızı giriniz: ");
+
+int yas= Convert.ToInt32(Console.ReadLine());
+
+if (yas < 18)
+{
+    Console.WriteLine("ehliyet alamaz");
+
+}
+else
+{
+    Console.WriteLine("ehliyet alabilir");
+}
+*/
+using System.Linq.Expressions;
+
+
+
+//if(islem== "+")
+//{
+//    int toplam = sayi1 + sayi2;
+//    Console.WriteLine($" {sayi1} ve {sayi2} nin toplamı : {toplam}");
+//}
+//else if( islem== "-")
+//{int sonuc= sayi1- sayi2;
+//    Console.WriteLine($" {sayi1} ve {sayi2} nin farkı : {sonuc}");
+
+//}
+//else
+//{
+//    Console.WriteLine("yanlış karakter girdiniz");
+
+//}
+//try
+//{
+//    Console.WriteLine("birinci sayıyı giriniz:");
+//    int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("+ ,- bunlardan birini seçiniz.");
+//    string islem = Console.ReadLine();
+
+//    Console.WriteLine("ikinci sayıyı giriniz:");
+//    int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+//    switch (islem)
+//    {
+//        case "+":
+//            {
+//                int toplam = sayi1 + sayi2;
+//                Console.WriteLine("sayıların toplamı " + toplam);
+//                break;
+//            }
+//        case "-":
+//            {
+//                int sonuc = sayi1 - sayi2;
+//                Console.WriteLine("sayıların farkı:" + sonuc);
+//                break;
+//            }
+//        default:
+//            {
+//                Console.WriteLine("yanlış karakter girdiniz");
+//                break;
+//            }
+//    }
+//}
+//catch (FormatException e)
+//{
+//    Console.WriteLine(e.Message);
+
+//}
+
+//int ilkDeger = 0;
+//while (ilkDeger<=10)
+//{
+//    Console.WriteLine(ilkDeger);
+//    ilkDeger += 3;
+//}
+
+ for(int i = 0;i<=10; i++)
+{
+    Console.WriteLine(i);
+}
---
+++ day3/Program.cs
@@ -0,0 +1,93 @@
+﻿//string ad = "Gülsu Doğan";
+//Console.WriteLine($" ad metninde ğ harfi var mı: {ad.Contains("ğ")}" );
+//Console.WriteLine( ad.ToUpper());
+
+////collections
+//using System.Net;
+//using System.Numerics;
+
+//string[] sehirler = { "Elazığ", "Kahramanmaraş", "Malatya","Ankara"};
+
+////Console.WriteLine(sehirler[0]);
+////Console.WriteLine(sehirler[1]);
+////Console.WriteLine(sehirler[2]);
+
+//for (int i=0; i<sehirler.Length;i++ )
+//{
+//    Console.WriteLine(sehirler[i]);
+//}
+//int[] sayilar = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//foreach(int sayi in sayilar)
+//{
+//    Console.WriteLine(sayi);
+//}
+
+
+//List<string>meyveler= new List<string>()
+//{
+//    "Karpuz","çilek","elma"
+//};
+//meyveler.Add("kavun");
+//meyveler.Add("armut");
+//List<string> eklenecekMeyveler = new List<string>()
+//{
+//    "ejderha meyvesi","mango"
+//};
+//meyveler.AddRange(eklenecekMeyveler);
+
+//foreach(string meyve in meyveler)
+//{
+//    Console.WriteLine(meyve);
+//}
+
+//meyveler.ForEach(meyve=> Console.WriteLine(meyve));
+
+//List<int> numbers = new()
+//{
+//    1,4,2,3,7,6,8,9,11,10
+//};
+//numbers.Sort(); // küçükten büyüğe sıralar
+//numbers.Reverse(); //büyükten küçüğe sıralar
+
+//numbers.ForEach(numbers=> Console.WriteLine(numbers));
+//numbers.Remove(7);  // listeden 7yi siler
+//Console.WriteLine($"listtedeki 3 olan değerin indeksi: {numbers.IndexOf(3)} ");
+//Console.WriteLine($"listedeki sayıların toplamı: {numbers.Sum()}");
+//Console.WriteLine($"listedeki sayıların en küçük sayı: {numbers.Min()}");
+//Console.WriteLine($"listedeki sayıların en büyük sayı: {numbers. Max()}");
+
+//List<int> numbers1 = new()
+//{
+//    1,4,2,3,7,6,8,9,11,10
+//};
+//List<int> evenNumbers = new();
+//foreach (int number in numbers1)
+//{
+//    if (number % 2 == 0)
+//    {
+//        evenNumbers.Add(number);
+//    }
+//}
+//foreach(int ciftsayi in evenNumbers)
+//{
+//    Console.WriteLine(ciftsayi);
+//}
+
+//evenNumbers = numbers1.FindAll(even=> even %2==0);
+//evenNumbers.ForEach(number=> Console.WriteLine(number));
+
+// sebzeler listesine 3 tane eleman ekleyin ve 3 elemanı listeleyin ve listede 2. indeksteki sebze adını ekrana bastırın
+// bu listede küçük harflerle yazılan sebzeleri büyük harflerle yazın
+
+List<string> sebzeler = new List<string>()
+{
+    "domates","biber","patlıcan"
+};
+foreach( string sebze in sebzeler)
+{
+    Console.WriteLine(sebze);
+
+}
+
+Console.WriteLine( sebzeler[2]);
+sebzeler.ForEach(sebze=> Console.WriteLine(sebze.ToUpper()));
